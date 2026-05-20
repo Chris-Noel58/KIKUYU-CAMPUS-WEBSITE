47ab6ea7723501c4c75ace5b5da21de75bede352
ChrisNoel58 <chrispinekipkurui@gmail.com>
2026-05-20 16:37:40 +0300

    sitemap update 3

--- urls.py
+++ urls.py
@@ -4,11 +4,18 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('website.urls', 'website'), namespace='website')),
-    path('dashboard/', include(('dashboard.urls', 'dashboard'), namespace='dashboard')),
-     path(
+    # serve sitemap at /sitemap.xml
+    path(
         "sitemap.xml",
         TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml"),
         name="sitemap",
     ),
+    # serve sitemap also at /sitemap (without .xml)
+    path(
+        "sitemap",
+        TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml"),
+        name="sitemap_noext",
+    ),
+    path('', include(('website.urls', 'website'), namespace='website')),
+    path('dashboard/', include(('dashboard.urls', 'dashboard'), namespace='dashboard')),
 ]
--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'website'
@@ -16,4 +17,15 @@ urlpatterns = [
     path('apply/', views.apply, name='apply'),
     path('contact/', views.contact, name='contact'),
     path('newsletter/subscribe/', views.newsletter_subscribe, name='newsletter_subscribe'),
+    # Sitemap routes
+    path(
+        "sitemap.xml",
+        TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml"),
+        name="sitemap",
+    ),
+    path(
+        "sitemap",
+        TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml"),
+        name="sitemap_noext",
+    ),
 ]
