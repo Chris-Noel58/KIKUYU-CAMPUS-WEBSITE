10ff0c5ebd3979eda5173e86fee74ddabf27bc6b
ChrisNoel58 <chrispinekipkurui@gmail.com>
2026-05-20 16:16:48 +0300

    sitemap update 2

--- urls.py
+++ urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('website.urls', 'website'), namespace='website')),
     path('dashboard/', include(('dashboard.urls', 'dashboard'), namespace='dashboard')),
+     path(
+        "sitemap.xml",
+        TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml"),
+        name="sitemap",
+    ),
 ]
